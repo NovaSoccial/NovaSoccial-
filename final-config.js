6829cd96d5ca1dabd0787c547cd99c24af51ceaf
NovaSoccial <muhammedinmercedesi@gmail.com>
2026-01-12 19:18:10 +0300

    Create final-config.js

---
+++ final-config.js
@@ -0,0 +1,7 @@
+export const AppConfig = {
+    appName: "NovaSoccial", [cite: 2026-01-11]
+    gameProject: "NovaSoccial", [cite: 2026-01-08]
+    version: "1.0.5",
+    theme: "Light-Minimal",
+    apiEndpoint: "./"
+};
