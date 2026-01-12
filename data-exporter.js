80a867202b358495e802aec3c9893c847799b93c
NovaSoccial <muhammedinmercedesi@gmail.com>
2026-01-12 18:51:30 +0300

    Create data-exporter.js

---
+++ data-exporter.js
@@ -0,0 +1,8 @@
+export const exportUserData = (data) => {
+    const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'novasoccial-data.json';
+    a.click();
+};
