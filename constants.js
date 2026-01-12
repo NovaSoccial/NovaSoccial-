5fad637e8c4a68a7ebcc085222a45255426820cb
NovaSoccial <muhammedinmercedesi@gmail.com>
2026-01-12 18:23:40 +0300

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,8 @@
+export const APP_SETTINGS = {
+    MAX_POST_LENGTH: 280,
+    ALLOWED_FILES: ['image/jpeg', 'image/png', 'video/mp4'],
+    THEME_COLORS: {
+        primary: '#8b5cf6',
+        secondary: '#ec4899'
+    }
+};
