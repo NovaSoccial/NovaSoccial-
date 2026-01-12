a098489a61c97874c94be888dfbcea05f9853d0f
NovaSoccial <muhammedinmercedesi@gmail.com>
2026-01-12 18:21:29 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,27 @@
-const CACHE_NAME = 'novasoccial-v1';
-const assets = ['/', '/index.html', '/style.css', '/app.js'];
+const CACHE_NAME = 'nova-v1';
+// Önbelleğe alınacak dosyalar
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Uygulama yüklenirken dosyaları hafızaya al
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// İnternet olmasa da hafızadaki dosyaları kullan
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
