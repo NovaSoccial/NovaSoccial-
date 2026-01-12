9d2b2a2fe703b8bd94e996dcd6a655b362f55c7b
NovaSoccial <muhammedinmercedesi@gmail.com>
2026-01-12 19:28:41 +0300

    Create image-picker.js

---
+++ image-picker.js
@@ -0,0 +1,12 @@
+export const pickImage = () => {
+    return new Promise((resolve) => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.onchange = (e) => {
+            const file = e.target.files[0];
+            if (file) resolve(file);
+        };
+        input.click();
+    });
+};
