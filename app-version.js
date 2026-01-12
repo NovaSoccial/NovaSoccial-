95aaaaa29d91d22e32c2beffb2823a04fa04f169
NovaSoccial <muhammedinmercedesi@gmail.com>
2026-01-12 18:52:49 +0300

    Update app-version.js

--- app-version.js
+++ app-version.js
@@ -2,5 +2,5 @@ export const APP_INFO = {
     version: "1.0.4",
     build: "2026.01",
     codename: "NovaSoccial", // [cite: 2026-01-11]
-    name: "WordZen Core" // [cite: 2026-01-08]
+    name: "NovaSoccial Core" // [cite: 2026-01-08]
 };
