c1685fd59efcccfe847311902f15ea03af0eb3f4
NovaSoccial <muhammedinmercedesi@gmail.com>
2026-01-12 17:57:49 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Uygulamanın Firebase bağlantı anahtarları
+export const firebaseConfig = {
+  apiKey: "AIzaSyBA1H0C7y4Cbt2ZUBtGRnvu-HgPv8F-iog",
+  authDomain: "novasoccial.firebaseapp.com",
+  databaseURL: "https://novasoccial-default-rtdb.firebaseio.com",
+  projectId: "novasoccial",
+  storageBucket: "novasoccial.firebasestorage.app",
+  messagingSenderId: "680106218526",
+  appId: "1:680106218526:web:0edcdbe46ff1c84582ca0f",
+  measurementId: "G-46LLVYES8C"
+};
